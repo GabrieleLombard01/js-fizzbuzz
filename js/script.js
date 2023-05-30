507d0a02adebd0dc0d2b978c8b7f11c48dcef7cb
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-05-30 17:06:22 +0200

    for added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
 //JS RESET
 console.log('JS OK')
+
+/*
+# Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero per i multipli di 5 stampi “Buzz” al posto del numero. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+
+​
+**QUANDO** apro la console
+- creo un contatore da 1 a 100
+- individuo i multipli di 3 
+- individuo i multipli di 5 
+- individuo i multipli sia di 3 che di 5
+
+    **QUANDO** stampo in console i numeri 
+
+    **SE** stampo i multipli di 3
+    - sostituisco i numeri con la parola FIZZ  
+
+    **SE** stampo i multipli di 5
+    - sostituisco i numeri con la parola Buzz
+
+    **SE** stampo i multipli sia di 3 che di 5
+    - sostituisco i numeri con la parola FIZZBUZZ
+
+**FINE**
+
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+|||NB: PER ARRIVARE A QUESTO RISULTATO CI SERVIREMO DI FOR E DI IF|||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+*/
+
+//QUANDO apro la console
+
+    //Creo un contatore da 1 a 100
+    for (let i = 1; i <= 100; i++){
+        console.log(i);
+    }
