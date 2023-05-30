862ff4d380cc85fde6c0e4578beea75a691800bd
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-05-30 17:30:46 +0200

    if, else if added

--- js/script.js
+++ js/script.js
@@ -30,9 +30,32 @@ console.log('JS OK')
 
 */
 
+
 //QUANDO apro la console
 
     //Creo un contatore da 1 a 100
     for (let i = 1; i <= 100; i++){
         console.log(i);
+
+        //individuo i multipli di 3 e 5
+            //sostituisco i numeri con la parola FizzBuzz
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz")
+
+        //individuo i multipli di 5
+            //sostituisco i numeri con la parola Buzz
+        } else if (i % 5 === 0) {
+            console.log("Buzz")
+
+        //individuo i multipli di 3
+            //sostituisco i numeri con la parola Fizz   
+        } else if (i % 3 === 0) {
+            console.log("Fizz")
+
+        //Tutti gli altri numeri    
+        } else {
+            console.log(i)
+        }
+
     }
+    
